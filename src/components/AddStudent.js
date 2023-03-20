839f5e3bc40dd86bc22e84e338a55e807a51a7e3
sejalpatel125 <91778994+sejalpatel125@users.noreply.github.com>
2023-03-20 23:01:36 +0530

    Add files via upload

--- src/components/AddStudent.js
+++ src/components/AddStudent.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
@@ -6,50 +6,41 @@ import { useDispatch } from "react-redux";
 import { createStudent } from "../actions/students";
 import DatePicker from "react-datepicker";
 import StudentDataService from "../services/StudentService";
-import moment from "moment";
 import AsyncSelect from 'react-select/async';
 
 import "react-datepicker/dist/react-datepicker.css";
-import Student from "./Student";
+//import Student from "./Student";
 
 import "../css/style.css";
 import UploadService from "../services/FileUploadService";
 
 
-const AddStudent = () => {  
+const AddStudent = () => {
   const [DOB, setDOB] = useState(new Date());
   const [gender, setGender] = useState(1);
   const [department, setDepartment] = useState([]);
 
-  const [selectedFiles, setSelectedFiles] = useState(undefined);
+  //const [selectedFiles, setSelectedFiles] = useState(undefined);
   const [profilePicture, setProfilePicture] = useState("");
-  const [progress, setProgress] = useState(0);
-  const [message, setMessage] = useState("");
-  const [fileInfos, setFileInfos] = useState([]);
+  //const [progress, setProgress] = useState(0);
+  //const [message, setMessage] = useState("");
+  //const [fileInfos, setFileInfos] = useState([]);
 
   const upload = (event) => {
     console.log("*****in Upload")
     let profilePictureFile = event.target.files;
-
-    setProgress(0);
     setProfilePicture(profilePictureFile[0]);
 
     UploadService.upload(profilePictureFile[0], (event) => {
-      setProgress(Math.round((100 * event.loaded) / event.total));
+
+    }).then((response) => {
+      console.log(response.data.files[0].fileUrl);
+      setProfilePicture(response.data.files[0].fileUrl);
+      console.log("****profilePicture***" + profilePicture)
     })
-      .then((response) => {
-        console.log(response.data.files[0].fileUrl);
-        setMessage(response.data.files[0].fileUrl);
-        setProfilePicture(response.data.files[0].fileUrl);
-        console.log("****profilePicture***" + profilePicture)
-      })
       .catch(() => {
-        setProgress(0);
-        setMessage("Could not upload the file!");
         setProfilePicture(undefined);
       });
-
-
   };
 
   const initialStudentState = {
@@ -65,38 +56,32 @@ const AddStudent = () => {
   };
   const [student, setStudent] = useState(initialStudentState);
   const [submitted, setSubmitted] = useState(false);
-  
-
   const dispatch = useDispatch();
- /*  useEffect(async () => {
-    
-  }, []); */
+  /*  useEffect(async () => {
+     
+   }, []); */
 
   const handleInputChange = event => {
     const { name, value } = event.target;
     setStudent({ ...student, [name]: value });
-  };  
-
-  
+  };
 
   const newStudent = () => {
     setStudent(initialStudentState);
     setSubmitted(false);
   };
 
-  const loadOptions = async (input, callBack)=>{
+  const loadOptions = async (input, callBack) => {
     const res = await StudentDataService.getDepartment(input);
     console.log("sdsd")
     console.log(res)
-    const formatedRes = res.data.map(i=>({label:i.text ,value:i.value}))
+    const formatedRes = res.data.map(i => ({ label: i.text, value: i.value }))
     console.log(formatedRes)
-    callBack(res.data.map(i=>({label:i.text ,value:i.value})))
-   
+    callBack(res.data.map(i => ({ label: i.text, value: i.value })))
   }
 
   const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
 
-  
   const validationSchema = Yup.object().shape({
     firstName: Yup.string().required('Firstname is required'),
     lastName: Yup.string().required('Lastame is required'),
@@ -118,15 +103,15 @@ const AddStudent = () => {
   } = useForm({
     resolver: yupResolver(validationSchema)
   });
-  const onSubmit   = data => {
+  const onSubmit = data => {
     console.log(JSON.stringify(data))
     console.log("profilePicture" + profilePicture)
     const {
       firstName,
       lastName,
       mobileNo,
-      email, 
-     } = student;
+      email,
+    } = student;
 
     dispatch(createStudent(
       firstName,
@@ -137,30 +122,29 @@ const AddStudent = () => {
       mobileNo,
       email,
       JSON.stringify(department)
-      )).then(data => {
-        setStudent({
-          id: data.id,
-          firstName: data.firstName,
-          lastName: data.lastName,
-          profilePicture: data.profilePicture,
-          DOB: data.DOB,
-          gender: data.gender,
-          mobileNo: data.mobileNo, 
-          email: data.email,
-          departments: data.department,
-        });
-        setSubmitted(true);
-        setProfilePicture("");
-        setDOB(new Date());
-        setDepartment([]);
-        reset()
-        console.log(student.firstName  + JSON.stringify(initialStudentState));
-      })
+    )).then(data => {
+      setStudent({
+        id: data.id,
+        firstName: data.firstName,
+        lastName: data.lastName,
+        profilePicture: data.profilePicture,
+        DOB: data.DOB,
+        gender: data.gender,
+        mobileNo: data.mobileNo,
+        email: data.email,
+        departments: data.department,
+      });
+      setSubmitted(true);
+      setProfilePicture("");
+      setDOB(new Date());
+      setDepartment([]);
+      reset()
+      console.log(student.firstName + JSON.stringify(initialStudentState));
+    })
       .catch(e => {
         console.log(e);
       });
   };
-  
 
   return (
     <div className="submit-form">
@@ -173,111 +157,109 @@ const AddStudent = () => {
         </div>
       ) : (
         <div className="register-form">
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="form-group">
-            <label htmlFor="title">FirstName</label>
-            <input
-              {...register('firstName')}
-              type="text"
-              className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
-              id="firstName"
-              
-              value={student.firstName}
-              onChange={handleInputChange}
-              name="firstName"
-            />
-            <div className="invalid-feedback">{errors.firstName?.message}</div>
-          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-group">
+              <label htmlFor="title">FirstName</label>
+              <input
+                {...register('firstName')}
+                type="text"
+                className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
+                id="firstName"
 
-          <div className="form-group">
-            <label htmlFor="description">LastName</label>
-            <input
-              type="text"
-              className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
-              id="lastName"
-              {...register('lastName')}
-              value={student.lastName}
-              onChange={handleInputChange}
-              name="lastName"
-            />
-            <div className="invalid-feedback">{errors.lastName?.message}</div>
-          </div>
-          <div className="form-group">
-            <label htmlFor="description">Email</label>
-            <input
-              type="text"
-              className={`form-control ${errors.email ? 'is-invalid' : ''}`}
-              id="email"
-              {...register('email')}
-              value={student.email}
-              onChange={handleInputChange}
-              name="email"
-            />
-            <div className="invalid-feedback">{errors.email?.message}</div>
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="description">MobileNO</label>
-            <input
-              type="text"
-              className={`form-control ${errors.mobileNo ? 'is-invalid' : ''}`}
-              id="mobileNo"
-              {...register('mobileNo')}
-              value={student.mobileNo}
-              onChange={handleInputChange}
-              name="mobileNo"
-            />
-            <div className="invalid-feedback">{errors.mobileNo?.message}</div>
-          </div>
-
-
-
-          <div className="form-group">
-            <label htmlFor="DOB">DOB</label>
-            <DatePicker className="form-control student-custom-input"
-            utcOffset="0"
-            selected={DOB} 
-            onChange={(date) => {
-              setDOB(date)
-              console.log(date)
-              }} 
-            dateFormat="MMM dd, yyyy"  
-            showYearDropdown
-            maxDate={new Date()}
-            />
-          </div>
-
-          <div className="form-group">
-            <label htmlFor="gender">Gender</label>
-            <div>
-              <input type="radio" value="1" name="gender" checked={gender === 1} onClick = {() => setGender(1)}/> Male &nbsp;
-              <input type="radio" value="2" name="gender" checked={gender === 2} onClick = {() => setGender(2)}/> Female
+                value={student.firstName}
+                onChange={handleInputChange}
+                name="firstName"
+              />
+              <div className="invalid-feedback">{errors.firstName?.message}</div>
             </div>
-          </div> 
-     
 
-          <div className="form-group">
-            <label htmlFor="description">Departments</label>
-            <AsyncSelect 
-            isMulti 
-            value={department} 
-            onChange={(data)=>{setDepartment(data)}} 
-            placeholder="Select Department" 
-            name="departments"
-            id="departments"
-            loadOptions={loadOptions} />
-          </div>
-          
-          <div className="form-group">
-            <label htmlFor="description">ProfilePicture</label>
-            
-              <input  name="profilePicture" id="profilePicture" type="file" onChange={upload} />
-            
-          </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </form>
+            <div className="form-group">
+              <label htmlFor="description">LastName</label>
+              <input
+                type="text"
+                className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
+                id="lastName"
+                {...register('lastName')}
+                value={student.lastName}
+                onChange={handleInputChange}
+                name="lastName"
+              />
+              <div className="invalid-feedback">{errors.lastName?.message}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                id="email"
+                {...register('email')}
+                value={student.email}
+                onChange={handleInputChange}
+                name="email"
+              />
+              <div className="invalid-feedback">{errors.email?.message}</div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="MobileNO">MobileNO</label>
+              <input
+                type="text"
+                className={`form-control ${errors.mobileNo ? 'is-invalid' : ''}`}
+                id="mobileNo"
+                {...register('mobileNo')}
+                value={student.mobileNo}
+                onChange={handleInputChange}
+                name="mobileNo"
+              />
+              <div className="invalid-feedback">{errors.mobileNo?.message}</div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="DOB">DOB</label>
+              <DatePicker className="form-control student-custom-input"
+                utcOffset="0"
+                selected={DOB}
+                onChange={(date) => {
+                  setDOB(date)
+                  console.log(date)
+                }}
+                dateFormat="MMM dd, yyyy"
+                showYearDropdown
+                maxDate={new Date()}
+              />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="gender">Gender</label>
+              <div>
+                <input type="radio" value="1" name="gender" checked={gender === 1} onClick={() => setGender(1)} /> Male &nbsp;
+                <input type="radio" value="2" name="gender" checked={gender === 2} onClick={() => setGender(2)} /> Female
+              </div>
+            </div>
+
+
+            <div className="form-group">
+              <label htmlFor="description">Departments</label>
+              <AsyncSelect
+                isMulti
+                value={department}
+                onChange={(data) => { setDepartment(data) }}
+                placeholder="Select Department"
+                name="departments"
+                id="departments"
+                loadOptions={loadOptions} />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">ProfilePicture</label>
+
+              <input name="profilePicture" id="profilePicture" type="file" onChange={upload} />
+
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Submit
+            </button>
+          </form>
         </div>
       )}
     </div>
--- src/components/Student.js
+++ src/components/Student.js
@@ -1,13 +1,20 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as Yup from 'yup';
+import AsyncSelect from 'react-select/async';
 import { updateStudent, deleteStudent } from "../actions/students";
 import StudentDataService from "../services/StudentService";
 import DatePicker from "react-datepicker";
-import moment from "moment";
 import "react-datepicker/dist/react-datepicker.css";
+import "../css/style.css";
+import UploadService from "../services/FileUploadService";
 
 const Student = (props) => {
   const [DOB, setDOB] = useState(new Date());
+  const [gender, setGender] = useState(new Date());
+  const [department, setDepartment] = useState([]);
   const initialStudentState = {
     id: null,
     firstname: "",
@@ -17,19 +24,71 @@ const Student = (props) => {
   };
   const [currentStudent, setCurrentStudent] = useState(initialStudentState);
   const [message, setMessage] = useState("");
+  const [profilePicture, setProfilePicture] = useState("");
 
   const dispatch = useDispatch();
+
+  const loadOptions = async (input, callBack) => {
+    const res = await StudentDataService.getDepartment(input);
+    console.log("sdsd")
+    console.log(res)
+    const formatedRes = res.data.map(i => ({ label: i.text, value: i.value }))
+    console.log(formatedRes)
+    callBack(res.data.map(i => ({ label: i.text, value: i.value })))
+  }
+
+  const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/
+
+  const validationSchema = Yup.object().shape({
+    firstName: Yup.string().required('Firstname is required'),
+    lastName: Yup.string().required('Lastame is required'),
+    email: Yup.string()
+      .required('Email is required')
+      .email('Email is invalid'),
+    mobileNo: Yup.string()
+      .matches(phoneRegExp, 'Phone number is not valid')
+      .min(10, 'Phone number is not valid')
+      .max(12, 'Phone number is not valid'),
+
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    resolver: yupResolver(validationSchema)
+  });
 
   const getStudent = id => {
     StudentDataService.get(id)
       .then(response => {
-        setDOB(response.data.DOB);
+        setDOB(new Date(response.data.DOB));
         setCurrentStudent(response.data);
+        setGender(response.data.gender);
+        setDepartment(JSON.parse(response.data.departments));
+        setProfilePicture(response.data.profilePicture);
         console.log(response.data);
-        alert(currentStudent.DOB)
       })
       .catch(e => {
         console.log(e);
+      });
+  };
+
+  const upload = (event) => {
+    console.log("*****in Upload")
+    let profilePictureFile = event.target.files;
+    setProfilePicture(profilePictureFile[0]);
+
+    UploadService.upload(profilePictureFile[0], (event) => {
+
+    }).then((response) => {
+      console.log(response.data.files[0].fileUrl);
+      setProfilePicture(response.data.files[0].fileUrl);
+      console.log("****profilePicture***" + profilePicture)
+    })
+      .catch(() => {
+        setProfilePicture(undefined);
       });
   };
 
@@ -40,22 +99,25 @@ const Student = (props) => {
   const handleInputChange = event => {
     const { name, value } = event.target;
     setCurrentStudent({ ...currentStudent, [name]: value });
-  };
-
-  
+  }; 
 
   const updateStatus = status => {
     const data = {
       id: currentStudent.id,
       firstname: currentStudent.firstname,
       lastname: currentStudent.lastname,
-      status: status
+      DOB: DOB,
+      profilePicture: profilePicture,
+      gender: gender,
+      mobileNo: currentStudent.mobileNo,
+      email:currentStudent.email,
+      departments: JSON.stringify(department),
+      status: currentStudent.status
     };
 
     dispatch(updateStudent(currentStudent.id, data))
       .then(response => {
         console.log(response);
-
         setCurrentStudent({ ...currentStudent, status: status });
         setMessage("The status was updated successfully!");
       })
@@ -91,61 +153,112 @@ const Student = (props) => {
       {currentStudent ? (
         <div className="edit-form">
           <h4>Student</h4>
-          <form>
+          <form onSubmit={handleSubmit(updateContent)}>
             <div className="form-group">
               <label htmlFor="firstname">FirstName</label>
               <input
+                {...register('firstName')}
                 type="text"
-                className="form-control"
+                className={`form-control ${errors.firstName ? 'is-invalid' : ''}`}
                 id="firstname"
                 name="firstname"
                 value={currentStudent.firstName}
                 onChange={handleInputChange}
               />
+              <div className="invalid-feedback">{errors.firstName?.message}</div>
             </div>
             <div className="form-group">
               <label htmlFor="lastname">LastName</label>
               <input
                 type="text"
-                className="form-control"
+                className={`form-control ${errors.lastName ? 'is-invalid' : ''}`}
                 id="lastname"
                 name="lastname"
+                {...register('lastName')}
                 value={currentStudent.lastName}
                 onChange={handleInputChange}
+              />
+              <div className="invalid-feedback">{errors.lastName?.message}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email</label>
+              <input
+                type="text"
+                className={`form-control ${errors.email ? 'is-invalid' : ''}`}
+                id="email"
+                {...register('email')}
+                value={currentStudent.email}
+                onChange={handleInputChange}
+                name="email"
+              />
+              <div className="invalid-feedback">{errors.email?.message}</div>
+            </div>
+            <div className="form-group">
+              <label htmlFor="MobileNO">MobileNO</label>
+              <input
+                type="text"
+                className={`form-control ${errors.mobileNo ? 'is-invalid' : ''}`}
+                id="mobileNo"
+                {...register('mobileNo')}
+                value={currentStudent.mobileNo}
+                onChange={handleInputChange}
+                name="mobileNo"
+              />
+              <div className="invalid-feedback">{errors.mobileNo?.message}</div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="DOB">DOB</label>
+              <DatePicker className="form-control student-custom-input"
+                utcOffset="0"
+                selected={DOB}
+                onChange={(date) => {
+                  setDOB(date)
+                  console.log(date)
+                }}
+                dateFormat="MMM dd, yyyy"
+                showYearDropdown
+                maxDate={new Date()}
               />
             </div>
 
             {/* <div className="form-group">
               <label>
-                <strong>Status:</strong>
-              </label>
-              {currentStudent.status ? "Status" : "Pending"}
-            </div> */}
-
-            <div className="form-group">
-              <label>
                 <strong>DOB:</strong>
               </label>
               <DatePicker selected={moment(DOB).toDate("YYYY-MM-DD HH:mm Z")} onChange={(date) => setDOB(date)}/>
+            </div> */}
+
+            <div className="form-group">
+              <label htmlFor="gender">Gender</label>
+              <div>
+                <input type="radio" value="1" name="gender" checked={gender === 1} onClick={() => setGender(1)} /> Male &nbsp;
+                <input type="radio" value="2" name="gender" checked={gender === 2} onClick={() => setGender(2)} /> Female
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">Departments</label>
+              <AsyncSelect
+                isMulti
+                value={department}
+                onChange={(data) => { setDepartment(data) }}
+                placeholder="Select Department"
+                name="departments"
+                id="departments"
+                loadOptions={loadOptions} />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="description">ProfilePicture</label>
+              <img src={profilePicture} width="75px" height="75px"/>
+              <input name="profilePicture" id="profilePicture" type="file" onChange={upload} />
+
             </div>
             
           </form>
 
-          {currentStudent.status ? (
-            <button
-              className="badge badge-primary mr-2"
-              onClick={() => updateStatus(false)}
-            >
-              UnPublish
-            </button>
-          ) : (
-            <button
-              className="badge badge-primary mr-2"
-              onClick={() => updateStatus(true)}
-            >
-              Publish
-            </button>
-          )}
+         
 
           <button className="badge badge-danger mr-2" onClick={removeStudent}>
             Delete
@@ -154,7 +267,6 @@ const Student = (props) => {
           <button
             type="submit"
             className="badge badge-success"
-            onClick={updateContent}
           >
             Update
           </button>
